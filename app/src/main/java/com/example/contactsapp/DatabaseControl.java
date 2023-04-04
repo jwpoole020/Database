52d64376979f3119bfff570a08bd7fb4e32c9902
android <asdf>
2023-04-04 11:19:51 -0400

    Initial

---
+++ app/src/main/java/com/example/contactsapp/DatabaseControl.java
@@ -0,0 +1,69 @@
+package com.example.contactsapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+public class DatabaseControl {
+     SQLiteDatabase database;
+     DatabaseHelper helper;
+
+     public DatabaseControl(Context context){
+          helper = new DatabaseHelper(context);
+     }
+
+     public void open(){
+          database = helper.getWritableDatabase();
+     }
+
+     public void close(){
+          helper.close();
+     }
+
+     public boolean insert(String name, String state){
+          ContentValues values = new ContentValues();
+          values.put("name", name);
+          values.put("state", state);
+          return database.insert("contact", null, values) >0;
+     }
+
+     public String getState(String name){
+          String querry = "select state from contact where name=\""+name+"\"";
+          Cursor cursor = database.rawQuery(querry, null);
+          cursor.moveToFirst();
+          String state = cursor.getString(0);
+          cursor.close();
+          return state;
+     }
+
+     public String[] getAllNamesArray(){
+          String query = "select name from contact";
+          Cursor cursor = database.rawQuery(query,null);
+          cursor.moveToFirst();
+          ArrayList<String> list = new ArrayList<String>();
+          while((!cursor.isAfterLast())){
+               String name = cursor.getString(0);
+               list.add(name);
+               cursor.moveToNext();
+          }
+          cursor.close();
+          String[] array = new String[list.size()];
+          return list.toArray(array);
+     }
+     public ArrayList<String> getAllNames(){
+          String query = "select name from contact";
+          Cursor cursor = database.rawQuery(query, null);
+          cursor.moveToFirst();
+          ArrayList<String> list = new ArrayList<String>();
+          while((!cursor.isAfterLast())){
+               String name = cursor.getString(0);
+               list.add(name);
+               cursor.moveToNext();
+          }
+          cursor.close();
+          return list;
+     }
+}
---
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.contactsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nameEdit;
+    Spinner spinner;
+    Button addButton;
+    Button getButton;
+    DatabaseControl control;
+    TextView result;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        control=new DatabaseControl(this);
+
+        nameEdit = findViewById(R.id.nameEdit);
+        spinner = findViewById(R.id.spinner);
+        addButton = findViewById(R.id.addButton);
+        getButton = findViewById(R.id.getButton);
+        result = findViewById(R.id.result);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                control.open();
+                String state = control.getState(nameEdit.getText().toString());
+                control.close();
+                result.setText(state);
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEdit.getText().toString();
+                String state = ((TextView) spinner.getSelectedView()).getText().toString();
+                control.open();
+                boolean itWorked = control.insert(name, state);
+                control.close();
+                if (itWorked)
+                    Toast.makeText(getApplicationContext(), "Added "+name+" "+state, Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Failed "+name+" "+state, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        control.open();
+        String[] data=control.getAllNamesArray();
+        control.close();
+
+        recyclerView.setAdapter(new CustomAdapter(data));
+    }
+}
